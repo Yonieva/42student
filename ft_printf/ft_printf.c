c014c2fe2934981f7f0a708cbf5e446fb2173df3
Yonieva <yonieva@student.42perpignan.fr>
2023-11-05 20:10:45 +0100

    ok

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,1 +1,36 @@
-ft
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonieva <yonieva@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 22:24:12 by yonieva           #+#    #+#             */
+/*   Updated: 2023/11/02 22:24:14 by yonieva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_printf(const char *format, ... )
+{
+	va_list	args;
+	int		i;
+	int		count;
+	
+	va_start(args, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+			count = count + print_format(format[i++], args);
+		else
+			count = count + write(1, format[i], 1);
+			i++;
+	}
+	va_end(args);
+	return count;
+}
