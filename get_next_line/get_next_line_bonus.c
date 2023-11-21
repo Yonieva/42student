89a74018fbf9232a45bb6e82589c995e3331b3ba
Yoan Onieva <yonieva@c2r4p8.42perpignan.fr>
2023-11-21 15:05:54 +0100

    ok avec bonus

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_read_save(int fd, char *save)
 {
