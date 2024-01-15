85892bb228880b8f6c20bc179dc949889f4b6716
Yoan Onieva <yonieva@c3r2p10.42perpignan.fr>
2024-01-15 18:41:49 +0100

    ok

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -12,40 +12,58 @@
 
 #include "so_long.h"
 
+
 int on_destroy(t_data *data)
 {
-	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	mlx_destroy_display(data->mlx_ptr);
-	free(data->mlx_ptr);
-	exit(0);
-	return (0);
+    mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+    mlx_destroy_display(data->mlx_ptr);
+    free(data->mlx_ptr);
+    exit(0);
+    return (0);
 }
- 
+
 int on_keypress(int keysym, t_data *data)
 {
-	(void)data;
-	printf("Pressed key: %d\\n", keysym);
-	return (0);
+    (void)data;
+    printf("Pressed key: %d\n", keysym);
+    return (0);
 }
- 
+
+
+
+
 int main(void)
 {
-	t_data data;
- 
-	data.mlx_ptr = mlx_init();
-	if (!data.mlx_ptr)
-		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 1080, 800, "METAL GEAR 42");
-	if (!data.win_ptr)
-		return (free(data.mlx_ptr), 1);
- 
-	// Enregistre touches relachees
-	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
- 
-	// Enregistre les destructions
-	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
- 
-	// Boucle sur le pointeur MLX
-	mlx_loop(data.mlx_ptr);
-	return (0);
+ t_data data;
+
+    data.mlx_ptr = mlx_init();
+    if (!data.mlx_ptr)
+        return (1);
+
+    data.win_ptr = mlx_new_window(data.mlx_ptr, 1080, 800, "METAL GEAR 42");
+    if (!data.win_ptr)
+        return (free(data.mlx_ptr), 1);
+
+    // Chargement de l'image
+    void *img;
+    char *relative_path = "./assets/map.xpm";
+    int img_width;
+    int img_height;
+
+    img = mlx_xpm_file_to_image(data.mlx_ptr, relative_path, &img_width, &img_height);
+    if (!img)
+        return (free(data.mlx_ptr), 1);
+
+    // Affichage de l'image
+    mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img, 0, 0);
+
+    // Enregistre touches relâchées
+    mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+
+    // Enregistre les destructions
+    mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+
+    // Boucle sur le pointeur MLX
+    mlx_loop(data.mlx_ptr);
+    return (0);
 }
