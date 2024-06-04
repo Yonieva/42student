2c249414e2b111d9525b584356ae4678f4ee2bf1
Yonieva <yonieva@student.42perpignan.fr>
2024-06-05 00:15:24 +0200

    ok

---
+++ philo/code/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonieva <yonieva@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 15:23:29 by yonieva           #+#    #+#             */
+/*   Updated: 2024/06/03 16:00:24 by yonieva          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <limits.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
